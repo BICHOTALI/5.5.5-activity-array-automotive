588c9637341a754c42dd1d0e2fcd0024b17e47cc
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-06-08 21:29:32 -0400

    added new instance of Vehicle class

--- mercurySedan.js
+++ mercurySedan.js
@@ -9,19 +9,64 @@ console.log(v.make)
 //After you write the derived Car class, you should test it out.
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
+class A28MercurySedan extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " doesnt have enough space to fit all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("out of gas!");
+            return this.started = false;
+        }
+    }
+    
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+
+
+}
 
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
 
+let myWhip = new A28MercurySedan('mercury', 'rad_sedan', '2002', 'white', 50000)
 
+myWhip.start()
+myWhip.loadPassenger(5)
+myWhip.stop()
+myWhip.checkService()
 
-
-
-
-
-
-
+console.log(myWhip)
 
 
 //TO DO: Creating Instances and Testing Them
